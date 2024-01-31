1f890a7e2efdba4e6d85ed1bc90cbc90c172226a
Nihang <nihang.rethinksoft@gmail.com>
2024-01-31 11:18:35 +0530

    Comment changes

--- index.js
+++ index.js
@@ -14,26 +14,37 @@ app.listen(3000, () => {
 });
 
 app.post("/result", async (req, res) => {
-  const { market_id, type } = req.body;
-  let data, res1;
-  if (type == "bookmakers") {
-    res1 = await axios.get('http://142.93.36.1/api/v1/marketResult?type=odds&market_id=' + market_id);
-    data = res1.data;
+  try {
+    const { market_id, type } = req.body;
+    if (!type) {
+      res.status(400).json({
+        message: 'Type is invalid.'
+      });
+    }
+    let data, res1;
+    if (type == "bookmakers") {
+      res1 = await axios.get('http://142.93.36.1/api/v1/marketResult?type=odds&market_id=' + market_id);
+      data = res1.data;
+    }
+    else if (type == "fancy") {
+      res1 = await axios.get('http://142.93.36.1/api/v1/marketResult?type=session&market_id=' + market_id);
+      data = res1.data;
+    }
+    else if (type == "fancy1") {
+      res1 = await axios.get('http://142.93.36.1/api/v1/marketResult?type=fancy1&market_id=' + market_id);
+      data = res1.data;
+    }
+    else {
+      res1 = await axios.get('http://142.93.36.1/api/v1/marketResult?type=odds&market_id=' + market_id);
+      data = res1.data;
+    }
+    res.status(200).json({
+      message: "Result get successfully..",
+      data: data
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: e.message
+    });
   }
-  else if (type == "fancy") {
-    res1 = await axios.get('http://142.93.36.1/api/v1/marketResult?type=session&market_id=' + market_id);
-    data = res1.data;
-  }
-  else if (type == "fancy1") {
-    res1 = await axios.get('http://142.93.36.1/api/v1/marketResult?type=fancy1&market_id=' + market_id);
-    data = res1.data;
-  }
-  else {
-    res1 = await axios.get('http://142.93.36.1/api/v1/marketResult?type=odds&market_id=' + market_id);
-    data = res1.data;
-  }
-  res.json({
-    message: "Result get successfully..",
-    data: data
-  });
 });
